b84f740f5623826f702b014b11434d2dbf81424f
leeejin <asd72621@naver.com>
2024-05-17 11:36:10 +0900

    TodoItem 구조분해 할당

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -2,21 +2,22 @@ import PropTypes from "prop-types";
 import checkIcon from "../../styles/images/check-icon.png";
 import deleteIcon from "../../styles/images/delete-icon.png";
 const TodoItem = ({ item, handleListDone, handleListDelete }) => {
+  const { title, content, id, isDone } = item; //구조분해 할당
   return (
     <div className="card-items">
-      <h5 className="list-title">{item.title}</h5>
-      <p className="list-content">{item.content}</p>
+      <h5 className="list-title">{title}</h5>
+      <p className="list-content">{content}</p>
       <div>
-        {!item.isDone && (
+        {!isDone && (
           <img
             src={checkIcon}
-            onClick={() => handleListDone(item.id)}
+            onClick={() => handleListDone(id)}
             width={"30px"}
           />
         )}
         <img
           src={deleteIcon}
-          onClick={() => handleListDelete(item.id)}
+          onClick={() => handleListDelete(id)}
           width={"30px"}
         />
       </div>
